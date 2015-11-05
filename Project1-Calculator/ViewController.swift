1d72cf72e15586ea16ee1425f5b2fae921dee170
Cassidy Wang <cjwang14@gmail.com>
2015-11-05 15:26:40 -0500

    Initial commit

--- Project1-Calculator/ViewController.swift
+++ Project1-Calculator/ViewController.swift
@@ -10,16 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsTyping = false
+    
+    var brain = CalculatorBrain()
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        if userIsTyping {
+            display.text = display.text! + sender.currentTitle!
+        } else {
+            display.text = sender.currentTitle!
+            userIsTyping = true
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func delete() {
+        if (display.text!).characters.count > 1 {
+            display.text!.removeAtIndex(display.text!.endIndex.predecessor())
+        } else {
+            display.text = "0"
+            userIsTyping = false
+        }
     }
-
+    
+    @IBAction func operate(sender: UIButton) {
+        if userIsTyping {
+            evaluate()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+    }
+    
+    @IBAction func evaluate() {
+        userIsTyping = false
+        if let result = brain.pushOperand(displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsTyping = false
+        }
+        
+    }
 
 }
-
