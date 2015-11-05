3fda3441bdb5d3b671318d9bb0e5d4ed414bc322
Cassidy Wang <cjwang14@gmail.com>
2015-11-05 15:35:45 -0500

    Added property list

--- Project1-Calculator/CalculatorBrain.swift
+++ Project1-Calculator/CalculatorBrain.swift
@@ -41,7 +41,26 @@ class CalculatorBrain
         knownOps["√"] = Op.UnaryOperation("√", sqrt)
     }
     
-    var program 
+    typealias PropertyList = AnyObject
+    
+    var program: PropertyList { //guaranteed to be a PropertyList
+        get {
+            return opStack.map( {$0.description} )
+        }
+        set {
+            if let opSymbols = newValue as? Array<String> {
+                var newOpStack = [Op]()
+                for opSymbol in opSymbols {
+                    if let op = knownOps[opSymbol] {
+                        newOpStack.append(op)
+                    } else if let operand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue {
+                        newOpStack.append(.Operand(operand))
+                    }
+                }
+                opStack = newOpStack
+            }
+        }
+    }
     
     private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op]) {
         if !ops.isEmpty {
